fd3a18df0c6cc1fc5900458f20158293cb460634
tcarrio <tom@carrio.me>
2016-11-24 18:01:52 -0500

    Using static cipher string after manual set function used for removing duplicates from random string generation

--- prog3.py
+++ prog3.py
@@ -2,6 +2,7 @@
 # Course: CSE-233
 
 import sys
+import pprint
 
 def setup_files():
     if(len(sys.argv)==4):
@@ -13,22 +14,30 @@ def setup_files():
         return
 
 def setup_ciphers():
-    letter='A'
-    cipher=list(set([c for c in "4ybdddifk&C!r1Nm6w6uuZK@Qc^5h%8Z3o$myCU6l!loGWdPK&EQzIVEC!".upper()]))
+    tmp_cipher=[c for c in "lpDzQ2FEnOJXCHAPc8Rb16j5i3LogGBU7Im0V9duYsyTa4KSxveh"]
     encCipher={}
-    for i in range(26):
-        encCipher[letter]=cipher[i]
-        letter=chr(ord(letter)+1)
-    decCipher={v:k for k,v in encCipher.items()}
+    decCipher={}
+    for letter,offset in {'a':0,'A':26}.items():
+        for i in range(26):
+            print(letter)
+            encCipher[letter]=tmp_cipher[i+offset]
+            decCipher[tmp_cipher[i+offset]]=letter
+            letter=chr(ord(letter)+1)
+    encCipher['__type']="encrypt"
+    decCipher['__type']="decrypt"
     return (encCipher,decCipher)
 
 def do_the_thing(cipher,readfile,writefile):
     f=open(readfile,'r')
     o=open(writefile,'w')
-    for c in f.read().upper():
+    fin=f.read()
+    print("File in:\n{}".format(fin))
+    for c in fin:
         o.write(cipher.get(c,c))
     f.close()
     o.close()
+    with open(writefile,'r') as fout:
+        print("File out:\n{}".format(fout.read()))
 
 def fail_message(msg=""):
     print("You have input functionality that is not supported")
@@ -42,9 +51,10 @@ def main():
     elif(sys.argv[1]=='decrypt'):
         this_cipher=setup_ciphers()[1]
     else:
-        fail_message()
+        fail_message("Not a valid option")
         return
     try:
+        print("The cipher type in use is '{}''".format(this_cipher['__type']))
         infile,outfile=setup_files()
         do_the_thing(this_cipher,infile,outfile)
     except Exception as e:
